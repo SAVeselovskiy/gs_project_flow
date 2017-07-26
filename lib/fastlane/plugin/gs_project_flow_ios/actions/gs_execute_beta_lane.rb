97c187d1652b301bc45ddc62b6668c864e777037
Сергей Веселовский <sergejveselovskij@MacBook-Pro-Sergej.local>
2017-07-26 15:36:41 +0300

    v0.2.8

--- lib/fastlane/plugin/gs_project_flow_ios/actions/gs_execute_beta_lane.rb
+++ lib/fastlane/plugin/gs_project_flow_ios/actions/gs_execute_beta_lane.rb
@@ -4,13 +4,14 @@ module Fastlane
       def self.run(params)
         require 'fastlane/plugin/gs_versioning'
         require 'fastlane/plugin/versioning'
+        require 'fastlanecore'
         # match(type: "appstore") # more information: https://codesigning.guide
         # Increment the build number (not the version number)
         UI.message("alias = " + params[:alias])
 
-        v = Helper::GsProjectFlowIosHelper.new.execute_action('gs_increment_beta_version', {path:Helper::GsProjectFlowIosHelper.get_versions_path})
-
-        # v = GsIncrementBetaVersionAction.run(path:Fastlane::Helper::GsProjectFlowIosHelper.get_versions_path)
+        # v = Helper::GsProjectFlowIosHelper.new.execute_action('gs_increment_beta_version', {path:Helper::GsProjectFlowIosHelper.get_versions_path})
+        arg = FastlaneCore::Configuration.create(GsIncrementBetaVersionAction.available_options, {path:Helper::GsProjectFlowIosHelper.get_versions_path})
+        v = GsIncrementBetaVersionAction.run(arg)
         version_name = v.major.to_s + "." + v.minor.to_s + "." + v.build.to_s
         Helper::GsProjectFlowIosHelper.new.execute_action('increment_build_number_in_plist', {xcodeproj:ENV["xcodeproj"],
                                                                                               target:ENV["target"],
@@ -21,8 +22,8 @@ module Fastlane
         # )
 
         Helper::GsProjectFlowIosHelper.new.execute_action('increment_build_version_in_plist', {version_number: v.major.to_s + "." + v.minor.to_s + ".0",
-                                                                                               xcodeproj: ENV["xcodeproj"],
-                                                                                               target: ENV["target"]})
+                                                                                                                                                                                                             xcodeproj: ENV["xcodeproj"],
+                                                                                                                                                                                                             target: ENV["target"]})
         # IncrementVersionNumberInPlist.run(
         #   version_number: v.major.to_s + "." + v.minor.to_s + ".0",
         #   xcodeproj: ENV["xcodeproj"],
