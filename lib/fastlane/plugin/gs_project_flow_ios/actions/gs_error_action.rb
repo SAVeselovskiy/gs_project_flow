333daa564b8aaf446a639917defc18f3e5983345
Сергей Веселовский <sergejveselovskij@MacBook-Pro-Sergej.local>
2017-08-18 11:20:20 +0300

    v0.4.17

--- lib/fastlane/plugin/gs_project_flow_ios/actions/gs_error_action.rb
+++ lib/fastlane/plugin/gs_project_flow_ios/actions/gs_error_action.rb
@@ -2,12 +2,17 @@ module Fastlane
   module Actions
     class GsErrorAction < Action
       def self.run(params)
-        UI.important('Im in plugin error')
-        version_name, v = Helper::GsProjectFlowIosHelper.version_for_lane(params[:lane], Helper::GsProjectFlowIosHelper::BuildState::FAILURE)
-        # ENV["PROJECT_NAME"] - переменка окружения, используемая в iOS, как читаемое название проекта + ключ в json файлике версий
-        message = ENV["PROJECT_NAME"] + " " + version_name + " build has failed. Reason:\n " + params[:exception].message
-        UI.important(message)
-        Helper::GsProjectFlowIosHelper.send_report(message,Helper::GsProjectFlowIosHelper::BuildState::FAILURE, params[:lane])
+        begin
+          version_name, v = Helper::GsProjectFlowIosHelper.version_for_lane(params[:lane], Helper::GsProjectFlowIosHelper::BuildState::FAILURE)
+          # ENV["PROJECT_NAME"] - переменка окружения, используемая в iOS, как читаемое название проекта + ключ в json файлике версий
+          message = ENV["PROJECT_NAME"] + " " + version_name + " build has failed. Reason:\n " + params[:exception].message
+
+          UI.important(message)
+          Helper::GsProjectFlowIosHelper.send_report(message,Helper::GsProjectFlowIosHelper::BuildState::FAILURE, params[:lane])
+        rescue => ex
+          message = "Build has failed on error lane. Reason main lane failed:\n " + params[:exception].message + " \n Reason error lane failed:\n" + ex.message
+          Helper::GsProjectFlowIosHelper.send_report(message,Helper::GsProjectFlowIosHelper::BuildState::FAILURE, params[:lane])
+        end
       end
 
       def self.description
--- lib/fastlane/plugin/gs_project_flow_ios/version.rb
+++ lib/fastlane/plugin/gs_project_flow_ios/version.rb
@@ -1,5 +1,5 @@
 module Fastlane
   module GsProjectFlowIos
-    VERSION = "0.4.16"
+    VERSION = "0.4.17"
   end
 end
